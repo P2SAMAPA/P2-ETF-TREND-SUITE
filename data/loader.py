37c12caeb8d09a5a637012e9005e8fe544377c0f
P2SAMAPA <pranabpattanaik@gmail.com>
2026-03-18 11:44:08 +0800

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -6,7 +6,7 @@ import os
 import streamlit as st
 
 # --- GLOBAL CONSTANTS ---
-X_EQUITY_TICKERS = ["XLK", "XLY", "XLP", "XLE", "XLV", "XLI", "XLB", "XLRE", "XLU", "XLC", "XLF", "XBI", "XME", "XOP", "XHB", "XSD", "XRT", "GDX", "XAR", "XNTK"]
+X_EQUITY_TICKERS = ["XLK", "XLY", "XLP", "XLE", "XLV", "XLI", "XLB", "XLRE", "XLU", "XLC", "XLF", "XBI", "XME", "XHB", "XSD", "XRT", "GDX", "XAR", "XNTK"]
 FI_TICKERS = ["TLT", "IEF", "TIP", "GLD", "SLV", "VGIT", "VCLT", "VCIT", "HYG", "PFF", "MBB", "VNQ", "LQD", "AGG"]
 REPO_ID = "P2SAMAPA/etf_trend_data"
 FILENAME = "market_data.csv"
